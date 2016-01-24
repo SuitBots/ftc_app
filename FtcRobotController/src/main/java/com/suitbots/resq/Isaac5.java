6ac3549f10c42da16718baeb3163a2fb3bc8b4c4
Corey Porter <cp@conduce.com>
2016-01-24 11:25:34 -0800

    WIP

--- FtcRobotController/src/main/java/com/suitbots/resq/Isaac5.java
+++ FtcRobotController/src/main/java/com/suitbots/resq/Isaac5.java
@@ -1,23 +1,19 @@
 package com.suitbots.resq;
 
-import android.bluetooth.BluetoothClass;
-import android.graphics.Color;
-
 import com.qualcomm.hardware.ModernRoboticsI2cGyro;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
-import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.robocol.Telemetry;
 
 public  class Isaac5  {
-    private DcMotor l1, l2, l3, r1, r2, r3;
-    private DcMotor tape;
-    private Servo Dumper_flipper;
+    private DcMotor l1, l2, r1, r2;
+    private DcMotor tape, winch;
+    private Servo dumper_flipper;
     private DeviceInterfaceModule dim;
 
     private ColorSensor color_fore, color_under;
@@ -31,20 +27,18 @@ public  class Isaac5  {
 
     Isaac5(HardwareMap hardwareMap, Telemetry _telemetry) throws InterruptedException {
         dim = hardwareMap.deviceInterfaceModule.get("dim");
-        l1 = hardwareMap.dcMotor.get("l1"); // y
-        l2 = hardwareMap.dcMotor.get("l2"); // y
-        l3 = hardwareMap.dcMotor.get("l3"); // y
-        r1 = hardwareMap.dcMotor.get("r1"); // y
-        r2 = hardwareMap.dcMotor.get("r2"); // y
-        r3 = hardwareMap.dcMotor.get("r3"); // y
-        // tape = hardwareMap.dcMotor.get("tape"); // y
+        l1 = hardwareMap.dcMotor.get("l1");
+        l2 = hardwareMap.dcMotor.get("l2");
+        r1 = hardwareMap.dcMotor.get("r1");
+        r2 = hardwareMap.dcMotor.get("r2");
+        tape = hardwareMap.dcMotor.get("tape");
+        winch = hardwareMap.dcMotor.get("winch");
 
         // Reverse the left motors
         r1.setDirection(DcMotor.Direction.REVERSE);
         r2.setDirection(DcMotor.Direction.REVERSE);
-        r3.setDirection(DcMotor.Direction.REVERSE);
 
-        Dumper_flipper = hardwareMap.servo.get("flipper"); // y
+        dumper_flipper = hardwareMap.servo.get("flipper"); // y
 
         telemetry = _telemetry;
         distance = hardwareMap.opticalDistanceSensor.get("distance"); // y
@@ -143,21 +137,56 @@ public  class Isaac5  {
     protected void setDriveMotorSpeeds(double left, double right) {
         l1.setPower(clamp(left));
         l2.setPower(clamp(left));
-        l3.setPower(clamp(left));
         r1.setPower(clamp(right));
         r2.setPower(clamp(right));
-        r3.setPower(clamp(right));
     }
 
-    protected void diagnosticSetDriveMotorSpeeds(double _l1, double _l2, double _l3,
-                                                 double _r1, double _r2, double _r3) {
+    protected void diagnosticSetDriveMotorSpeeds(double _l1, double _l2,
+                                                 double _r1, double _r2) {
         l1.setPower(_l1);
         l2.setPower(_l2);
-        l3.setPower(_l3);
         r1.setPower(_r1);
         r2.setPower(_r2);
-        r3.setPower(_r3);
     }
+
+    public void resetEncoders() {
+        l1.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        l2.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        r1.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        r2.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+    }
+
+    public void setEncoderTargets(int x) {
+        l1.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        l2.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        r1.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        r2.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        l1.setTargetPosition(x);
+        l2.setTargetPosition(x);
+        r1.setTargetPosition(x);
+        r2.setTargetPosition(x);
+    }
+
+    public void reetEncoderMode() {
+        l1.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        l2.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        r1.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        r2.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+    }
+
+    public boolean encodersAreZero() {
+        return l1.getCurrentPosition() == 0 &&
+                l2.getCurrentPosition() == 0 &&
+                r1.getCurrentPosition() == 0 &&
+                r2.getCurrentPosition() == 0;
+    }
+
+    public boolean motorsAreBusy() {
+        return l1.isBusy() || l2.isBusy() ||
+                r1.isBusy() || r2.isBusy();
+    }
+
+
 
     /// Get the heading from the gyro sensor
     int getHeading() { return gyro.getHeading(); }
@@ -190,7 +219,7 @@ public  class Isaac5  {
     }
 
     public static final double FULL_SPEED = 1.0;
-    public static final double SLOW_SPEED = 0.2;
+    public static final double SLOW_SPEED = 0.5;
 
     /// Full speed left turn
     public void turnLeft() {
@@ -212,6 +241,12 @@ public  class Isaac5  {
     public void goForward() {
         setDriveMotorSpeeds(FULL_SPEED, FULL_SPEED);
     }
+
+    /// Creep ahead
+    public void goForwardSlowly() {
+        setDriveMotorSpeeds(SLOW_SPEED, SLOW_SPEED);
+    }
+    
     /// Full speed backwards
     public void goBackward() {
         setDriveMotorSpeeds(-FULL_SPEED, -FULL_SPEED);
@@ -235,15 +270,19 @@ public  class Isaac5  {
     public int getAlphaUnder() { return color_under.alpha(); }
 
     public void moveDumperArmToThrowPosition() {
-        Dumper_flipper.setPosition(1.0);
+        dumper_flipper.setPosition(1.0);
     }
 
     public void resetDumperArm() {
-        Dumper_flipper.setPosition(0.0);
+        dumper_flipper.setPosition(0.0);
     }
 
     public void setTapeMotor (double x) {
-        // tape.setPower(clamp(x));
+        tape.setPower(clamp(x));
+    }
+
+    public void setWinchMotor (double x) {
+        winch.setPower(clamp(x));
     }
 
 }
