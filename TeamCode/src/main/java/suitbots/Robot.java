5937cf41a0088930fedd798c30aa687dbfabd12c
MegaChocoManiac <boo4813@gmail.com>
2017-09-02 13:39:07 -0700

    Robot base code

---
+++ TeamCode/src/main/java/suitbots/Robot.java
@@ -0,0 +1,16 @@
+package suitbots;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by Samantha on 9/2/2017.
+ */
+
+public class Robot {
+    //Yo necesito dormir.
+    private BNO055IMU gyro;
+    private DcMotor lf, lr, rf, rr;
+    private ColorSensor lineDetector;
+}
