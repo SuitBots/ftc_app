6f5bf3820f259d78dbfcfe13ff246f54129a0bf7
MegaChocoManiac <boo4813@gmail.com>
2017-10-21 13:17:11 -0700

    Saturday: Autonomus (IM TIERED!! NEED SLEEP) #0002 working on Macine learning (need to get it to java)

--- TeamCode/src/main/java/suitbots/DataCollectionTeleOp.java
+++ TeamCode/src/main/java/suitbots/DataCollectionTeleOp.java
@@ -1,9 +1,18 @@
 package suitbots;
 
+import android.os.Environment;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.suitbots.util.Controller;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by Samantha on 10/21/2017.
@@ -15,6 +24,11 @@ public class DataCollectionTeleOp extends OpMode {
     private Controller g1, g2;
     private boolean debug_mode = false;
     private boolean near;
+    private PrintWriter printf;
+    public static final File ROOT_FOLDER = Environment.getExternalStorageDirectory();
+    public static final File FIRST_FOLDER = new File(ROOT_FOLDER + "/FIRST/");
+    private int countB = 1;
+    private int countR = 1;
 
 
     public void getColor() {
@@ -23,25 +37,48 @@ public class DataCollectionTeleOp extends OpMode {
 
     @Override
     public void init() {
+        try {
+            final File fp = new File(FIRST_FOLDER, "observations.txt");
+            printf = new PrintWriter(new FileWriter(fp));
+        } catch(IOException ioe){ }
+
         jewelColorDetector = hardwareMap.colorSensor.get("jewelColorDetector");
         g1 = new Controller(gamepad1);
         g2 = new Controller(gamepad2);
     }
 
-
-
+    @Override
+    public void stop(){
+        super.stop();
+        printf.close();
+    }
 
     @Override
     public void loop() {
-//        if(g1.XOnce()){
-//
-//        }else if(g1.BOnce()){
-//
-//        }
-        telemetry.addData("Color Alpha - ",jewelColorDetector.alpha());
-        telemetry.addData("Color Blue - ",jewelColorDetector.blue());
+        if(g1.X()){
+            printf.printf("%d\t%d\t%d\t%d\t%s\n",
+                    jewelColorDetector.red(),
+                    jewelColorDetector.green(),
+                    jewelColorDetector.blue(),
+                    jewelColorDetector.alpha(),
+                    "BLUE"
+                    );
+            telemetry.addData("Writing", "BLUE");
+            telemetry.addData("Counter BLUE", countB++);
+        } else if(g1.B()){
+            printf.printf("%d\t%d\t%d\t%d\t%s\n",
+                    jewelColorDetector.red(),
+                    jewelColorDetector.green(),
+                    jewelColorDetector.blue(),
+                    jewelColorDetector.alpha(),
+                    "RED");
+            telemetry.addData("Writing", "RED");
+            telemetry.addData("Counter RED", countR++);
+        }
         telemetry.addData("Color Red - ",jewelColorDetector.red());
         telemetry.addData("Color Green - ",jewelColorDetector.green());
+        telemetry.addData("Color Blue - ",jewelColorDetector.blue());
+        telemetry.addData("Color Alpha - ",jewelColorDetector.alpha());
 
         g1.update();
         g2.update();
