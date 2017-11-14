f5e1f589108ab1b83ceed8ce19269ee1f9204709
Jonah Simmons <jonahksimmons@gmail.com>
2017-11-14 15:21:26 -0800

    Merge in Jonah's work

--- TeamCode/src/main/java/suitbots/AutoBase.java
+++ TeamCode/src/main/java/suitbots/AutoBase.java
@@ -82,6 +82,11 @@ public abstract class AutoBase extends LinearOpMode  {
         robot.stopDriveMotors();
     }
 
+    protected void turnRad(final double r) throws InterruptedException {
+        robot.resetGyro();
+        turnToAngleRad(r);
+    }
+
     protected void driveDirectionTiles(double directionRadians, double tiles) throws InterruptedException {
         driveDirectionTiles(directionRadians, tiles, .35);
     }
