13deb6f73d5c196ea3e05974aea5c7bccb85245d
Randy Tallyup <suitbots@gmail.com>
2018-11-21 09:56:21 -0800

    new Vuforia key for webcams

---
+++ TeamCode/src/main/java/suitbots/VuforiaKey.java
@@ -0,0 +1,5 @@
+package suitbots;
+
+public class VuforiaKey {
+    public static final String VUFORIA_KEY = "AfwEvoT/////AAABmYf6ZjL3JEzEh0APp0eBhpEot8DZzWSWYGrs6vm7EI5aS+1BwhyiwP78yNQvfjpMCxAHS418h3RqKL2S0vCIACpHTJE6jweyrEXXM6eG3RN0eXKgyPoVFrZ1irGEKkWamRH5x3DwH/GE8Zxmr7/U3vNkKF96p0x9yC75J9GotGZH6EdiDYFhOmtu8jXFf6DM2Z/3cbtNBEo+aVjxKwo8bcCGMp3iE9RiVT3zuKuplNU7cG8RF5AnBBShQ7cIAGL4ug4HqEiLV8eUxCBL/3OpUWRVw4YnHvceid4vhs8OGbnGlSQAcZQGWxLocx1pXfKGF82heBg6D4NagMb0A5Woij5PNlszN6udKdghatTAQjTo";
+}
