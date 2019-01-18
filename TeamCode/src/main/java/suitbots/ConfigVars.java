fe8b78b619ba3bfd0e19c797c4f8bb09a5e2cc40
Randy Tallyup <suitbots@gmail.com>
2019-01-17 16:06:27 -0800

    pre-meet3

--- TeamCode/src/main/java/suitbots/ConfigVars.java
+++ TeamCode/src/main/java/suitbots/ConfigVars.java
@@ -11,8 +11,8 @@ public class ConfigVars {
     public static double SLOW_TURN_SPEED = .1;
     public static double TURNING_FUDGE_FACTOR = 2.0;
 
-    public static double ARM_UP_SPEED = 1.0;
-    public static double ARM_DOWN_SPEED = .85;
+    public static double ARM_UP_SPEED = 0.9;
+    public static double ARM_DOWN_SPEED = 1.0;
     public static double ARM_UP_SPEED_FINAL = .3;
     public static double ARM_UP_PERCENT_SLOW = .7;
 
@@ -23,18 +23,19 @@ public class ConfigVars {
     public static double INCHES_FROM_WALL = 4.0;
 
     /// Non-fast speed to drive during Teleop
-    public static double TELEOP_SLOW_SPEED = .35;
+    public static double TELEOP_SLOW_SPEED = .45;
 
     /// Position of the dump arm before dumping occurs
-    public static int TELEOP_ARM_UP = -465;
+    public static int TELEOP_ARM_UP = -445;
     /// Position of the dump arm in the resting position
     public static int TELEOP_ARM_DOWN = 0;
     /// Position of the dump servo when dumping
-    public static double TELEOP_DUMP_SERVO_POSITION = .6;
+    public static double TELEOP_DUMP_SERVO_POSITION = .55;
     /// Proportion of the dump servo's full range with which to move over time
     public static double TELEOP_DUMP_SERVO_LIMIT = .7;
 
     public static double SERVO_ANGLE_EXPONENTIAL = 1.0;
 
-    public static double HARVESTER_SCALE_FACTOR = .75;
+    public static double HARVESTER_SCALE_FACTOR = .7
+            ;
 }
